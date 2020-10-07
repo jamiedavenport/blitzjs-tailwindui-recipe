d1ca545d2496c51fbab2331f2c9d7dde4dfbbd91
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-10-07 17:23:32 +0100

    Add Inter to the default sans font

--- templates/config/tailwind.config.js
+++ templates/config/tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   future: "all",
   purge: ["{app,pages}/**/*.{js,jsx,ts,tsx}"],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
   variants: {},
   plugins: [require("@tailwindcss/ui")],
 };
